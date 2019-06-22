d6f2d3f2a945da8b812306fbdac055f73714878b
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-22 18:49:35 -0500

    reversing string with StringBuilder

---
+++ reverseStringWithStringBuilder.java
@@ -0,0 +1,10 @@
+public class reverseStringWithStringBuilder{
+	public static void main(String[]args){
+    // reverse string with StringBuilder
+    String s = "abc";
+    StringBuilder sb = new StringBuilder(s);
+
+    sb.reverse();
+    System.out.println("original string: " + s + "\nreversed string: " + sb);
+  }
+}
