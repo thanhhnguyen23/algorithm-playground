540e5a3c70b0aa30144dff444d4a14bd04fd7dbc
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2019-06-23 18:15:23 -0500

    using stacks

---
+++ ValidParenthesis.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParenthesis{
+    public static void main(String[]args){
+//        Boolean myString = isValid("{{}");
+//        System.out.println(myString);
+    }
+
+    private static boolean isValid(String s){
+//        HashMap<Character, Character> map = new HashMap<Character, Character>();
+//        map.put('(', ')');
+//        map.put('[', ']');
+//        map.put('{', '}');
+//        
+//        Stack<Character> stack = new Stack<Character>();
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            char curr = s.charAt(i);
+//        
+//            if (map.keySet().contains(curr)) {
+//                stack.push(curr);
+//            } else if (map.values().contains(curr)) {
+//                if (!stack.empty() && map.get(stack.peek()) == curr) {
+//                    stack.pop();
+//                } else {
+//                    return false;
+//                }
+//            }
+//        }
+//        return stack.empty();
+    }
+}
